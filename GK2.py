3adc9fb10a29efbea90da9622541816a7eae1276
tav9122 <vutrananh04@gmail>
2023-03-12 15:11:11 +0700

    .

--- GK2.py
+++ GK2.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import medfilt
 
+
 def read_audio(audio_file_path):
     audio_samples, sample_rate = sf.read(audio_file_path)
     return audio_samples, sample_rate
@@ -28,19 +29,31 @@ def calculate_ste_and_zcr(audio_samples, frame_size, hop_size):
 
 def segment_audio(ste, zcr, ste_threshold, zcr_threshold):
     voiced_unvoiced_segments = dict()
+    current_segment_start = None
     for i in range(len(ste)):
         if ste[i] > ste_threshold and zcr[i] < zcr_threshold:
             voiced_unvoiced_segments[i] = "green"
+            current_segment_start = None
+        elif ste[i] <= ste_threshold:
+            voiced_unvoiced_segments[i] = "red"
+            if current_segment_start is None:
+                current_segment_start = i
+            if (i - current_segment_start) >= 23\
+                    and (i == len(ste) - 1 or (ste[i + 1] > ste_threshold)):
+                for j in range(current_segment_start, i + 1):
+                    voiced_unvoiced_segments[j] = "blue"
+                current_segment_start = None
         else:
             voiced_unvoiced_segments[i] = "red"
+            current_segment_start = None
     return voiced_unvoiced_segments
 
 
 def filter_data(voiced_unvoiced_segments, kernel_size):
-    color_to_value = {"red": 1, "green": 0}
+    color_to_value = {"red": 1, "green": 0, "blue": 2}
     data_as_numbers = list(map(lambda x: color_to_value[x], voiced_unvoiced_segments.values()))
     temp_list = medfilt(data_as_numbers, kernel_size=kernel_size)
-    value_to_color = {1: "red", 0: "green"}
+    value_to_color = {1: "red", 0: "green", 2: "blue"}
     numbers_as_data = list(map(lambda x: value_to_color[x], temp_list))
     voiced_unvoiced_segments.update(zip(voiced_unvoiced_segments.keys(), numbers_as_data))
     return voiced_unvoiced_segments
@@ -53,24 +66,34 @@ def plot_results(audio_samples, sample_rate, ste, zcr, voiced_unvoiced_segments)
     audio_file_name = audio_file_path.split('/')[-1]
     fig.suptitle(audio_file_name, fontsize=16)
 
-    axs[0].plot(time, ste, label='STE', color='blue')
+    axs[0].plot(time, ste, label='STE', color='yellow')
     axs[0].plot(time, zcr, label='ZCR', color='purple')
     axs[0].set_xticks(np.arange(0, time[-1], 0.1))
     axs[0].set_yticks(np.arange(0, 1.1, 0.1))
 
     for frame_idx, color in voiced_unvoiced_segments.items():
-        axs[0].axvspan(frame_idx * hop_size / sample_rate,
-                       (frame_idx + 1) * hop_size / sample_rate,
-                       alpha=0.2, color=color)
+        if color == "blue":
+            axs[0].axvspan(frame_idx * hop_size / sample_rate,
+                           (frame_idx + 1) * hop_size / sample_rate,
+                           alpha=0.2, color=color)
+        else:
+            axs[0].axvspan(frame_idx * hop_size / sample_rate,
+                           (frame_idx + 1) * hop_size / sample_rate,
+                           alpha=0.2, color=color)
 
     axs[0].legend()
 
     time = np.arange(len(audio_samples)) / sample_rate
     axs[1].plot(time, audio_samples, color='black')
     for frame_idx, color in voiced_unvoiced_segments.items():
-        axs[1].axvspan(frame_idx * hop_size / sample_rate,
-                       (frame_idx + 1) * hop_size / sample_rate,
-                       alpha=0.2, color=color)
+        if color == "blue":
+            axs[1].axvspan(frame_idx * hop_size / sample_rate,
+                           (frame_idx + 1) * hop_size / sample_rate,
+                           alpha=0.2, color=color)
+        else:
+            axs[1].axvspan(frame_idx * hop_size / sample_rate,
+                           (frame_idx + 1) * hop_size / sample_rate,
+                           alpha=0.2, color=color)
 
     axs[1].set_xlabel('Time (s)')
     axs[1].set_ylabel('Amplitude')
@@ -80,7 +103,7 @@ def plot_results(audio_samples, sample_rate, ste, zcr, voiced_unvoiced_segments)
 
 
 if __name__ == "__main__":
-    audio_file_paths = ['./TinHieuKiemThu/phone_M1.wav']
+    audio_file_paths = ['./TinHieuKiemThu/studio_M1.wav']
     for audio_file_path in audio_file_paths:
         audio_samples, sample_rate = read_audio(audio_file_path)
 
@@ -88,8 +111,8 @@ if __name__ == "__main__":
 
         ste, zcr = calculate_ste_and_zcr(audio_samples, frame_size, hop_size)
 
-        voiced_unvoiced_segments = segment_audio(ste, zcr, ste_threshold=0.004, zcr_threshold=0.6)
+        voiced_unvoiced_segments = segment_audio(ste, zcr, ste_threshold=0.0015, zcr_threshold=0.12)
 
-        voiced_unvoiced_segments = filter_data(voiced_unvoiced_segments, kernel_size=9)
+        voiced_unvoiced_segments = filter_data(voiced_unvoiced_segments, kernel_size=5)
 
         plot_results(audio_samples, sample_rate, ste, zcr, voiced_unvoiced_segments)
